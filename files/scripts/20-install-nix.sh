5c6a43caac8ee33ebc97df74aa01c2962d7c05cc
Wulf C. Krueger <wk@mailstation.de>
2026-03-22 16:10:12 +0100

    Just use Fedora's nix.

--- files/scripts/20-install-nix.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# Non-interactive install with defaults.
-curl -fsSL https://install.determinate.systems/nix | sh -s -- install --no-confirm -- --no-start-daemon
